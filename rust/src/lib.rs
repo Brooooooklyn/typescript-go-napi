f52822b0ef8b87b8bad1ce27cb4977a81b84e7f2
LongYinan <lynweklm@gmail.com>
2025-03-12 15:54:22 +0800

    Add RunProject binding

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,26 +1,33 @@
 use std::ffi::CString;
+use std::ptr;
 
 use napi::bindgen_prelude::*;
 use napi_derive::napi;
 
 mod sys {
-    use std::ffi::c_char;
+    use std::ffi::{c_char, c_void};
 
     #[link(name = "ts", kind = "static", modifiers = "+bundle,+whole-archive")]
     unsafe extern "C" {
-        pub fn ResolveTsconfig(project: *const c_char) -> *mut c_char;
+        pub fn RunProject(
+            project: *const c_char,
+            diagnostics: *mut *mut c_void,
+            length: *mut usize,
+        );
     }
 }
 
 #[napi]
-pub fn resolve_tsconfig(project: String) -> Result<RawCString> {
-    let resolved = unsafe { sys::ResolveTsconfig(CString::new(project)?.as_ptr()) };
-    if resolved.is_null() {
-        Err(Error::new(
-            Status::GenericFailure,
-            "Failed to resolve tsconfig",
-        ))
-    } else {
-        Ok(RawCString::new(resolved, NAPI_AUTO_LENGTH))
-    }
+pub fn run_project(project: String) -> Result<()> {
+    let mut diagnostics = ptr::null_mut();
+    let mut length = 0;
+    unsafe {
+        sys::RunProject(
+            CString::new(project)?.as_ptr(),
+            &mut diagnostics,
+            &mut length,
+        )
+    };
+
+    Ok(())
 }
